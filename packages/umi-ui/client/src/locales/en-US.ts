1905c98843db39db6caf2cd5e398db5dd310108c
ycjcl868 <45808948@qq.com>
2019-08-02 16:16:23 +0800

    feat: ui project

--- packages/umi-ui/client/src/locales/en-US.ts
+++ packages/umi-ui/client/src/locales/en-US.ts
@@ -2,4 +2,6 @@ export default {
   'org.umi.ui.global.panel.home': 'Home',
   'org.umi.ui.global.panel.lang': 'language',
   'org.umi.ui.global.content.hello': 'Hello',
+  'org.umi.ui.global.project.create.title': 'Create Project',
+  'org.umi.ui.global.project.import.title': 'Import Project',
 };
